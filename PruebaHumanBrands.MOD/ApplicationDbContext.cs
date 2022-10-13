92f0467f26dd1e29d8731fc781b3b49b6e15b3ec
Moisés Yax <43942212+moisesy@users.noreply.github.com>
2022-10-12 21:47:10 -0600

    Upload DAL y MOD

---
+++ PruebaHumanBrands.MOD/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaHumanBrands.MOD.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaHumanBrands.MOD
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
---
+++ PruebaHumanBrands.MOD/ResponseData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaHumanBrands.MOD
+{
+    public class ResponseData
+    {
+        public int ResultCode { get; set; }
+        public string ResultMessage { get; set; }
+
+        public ResponseData()
+        {
+        }
+    }
+}
